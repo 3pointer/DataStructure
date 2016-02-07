31babcf32142d15aa4953fa394fbc7fd199781c5
[3pointer] <[3pointer@gmail.com]>
2016-02-07 20:26:41 +0800

    finish RB insert

--- RedBlack.cc
+++ RedBlack.cc
@@ -13,12 +13,12 @@ private:
         bool color;
         node * Left, * Right, * parent;
 
-        node (){
+        node (int x){
             color = RED;
             Left = NULL;
             Right = NULL;
             parent = NULL;
-            val = 0;
+            val = x;
         }
 
         node * grandparent(){
@@ -55,7 +55,7 @@ private:
         node *y = p -> Right;
 
         fa -> Left = y;
-        if (y != NULL){
+        if (y != NIL){
             y -> parent = fa;
         }
         p -> Right = fa;
@@ -81,7 +81,7 @@ private:
         node *y = p -> Left;
 
         fa -> Right = y;
-        if (y != NULL){
+        if (y != NIL){
             y -> parent = fa;
         }
         p -> Left = fa;
@@ -99,7 +99,112 @@ private:
             }
         }
 
+    }
 
+    void inorder(node *p) {
+        if (p == NIL){
+            return;
+        }
+        inorder(p -> Left);
+        cout << p -> val << endl;
+        inorder(p -> Right);
+    }
+
+    string outputColor(bool color){
+        return color ? "BLACK" : "RED";
+    }
+
+    node * getSmallestChild(node *p){
+        if (p -> Left == NIL){
+            return p;
+        }
+        return getSmallestChild(p -> Left);
+    }
+
+    void insert(node *p, int data){
+        if (p -> val >= data){
+            if (p -> Left != NIL){
+                insert(p -> Left, data);
+            } else {
+                node * tmp = new node(data);
+                tmp -> parent = p;
+                tmp -> Left = tmp -> Right = NIL;
+                p -> Left = tmp;
+                insert_case(tmp);
+            }
+        } else {
+            if (p -> Right != NIL){
+                insert(p -> Right, data);
+            } else {
+                node * tmp = new node(data);
+                tmp -> parent = p;
+                tmp -> Left = tmp -> Right = NIL;
+                p -> Right = tmp;
+                insert_case(tmp);
+            }
+        }
+    }
+
+    void insert_case(node *p){
+        if (p -> parent == NULL){           // 第一种情况 root 的情况 这里不可能发生
+            root = p;
+            p -> color = BLACK;
+            return;
+        }
+
+        if (p -> parent -> color == RED){
+            if (p -> uncle() -> color == RED){
+                p -> parent -> color = p -> uncle() -> color = BLACK;
+                p -> grandparent() -> color = RED;
+                insert_case(p -> grandparent());
+            } else {
+                if (p -> parent -> Right == p && p -> grandparent() -> Left == p -> parent){
+                    rotate_left(p);
+                    rotate_right(p);
+                    p -> color = BLACK;
+                    p -> Left = p -> Right = RED;
+                } else if (p -> parent -> Left == p && p -> grandparent() -> Right == p -> parent){
+                    rotate_right(p);
+                    rotate_left(p);
+                    p -> color = BLACK;
+                    p -> Left -> color = p -> Right -> color = RED;
+                } else if (p -> parent -> Left == p && p -> grandparent() -> Left == p -> parent){
+                    p -> parent -> color = BLACK;
+                    p -> grandparent() -> color = RED;
+                    rotate_right(p -> parent);
+                } else if (p -> parent -> Right == p && p -> grandparent() -> Right == p -> parent){
+                    p -> parent -> color = BLACK;
+                    p -> grandparent() -> color = RED;
+                    rotate_left(p -> parent);
+                }
+            }
+        }
+    }
+
+    void DeleteTree(node *p){
+        if (p == NULL){
+            return ;
+        }
+        DeleteTree(p -> Left);
+        DeleteTree(p -> Right);
+        delete p;
+    }
+
+public:
+    bst(){
+        NIL = new node(0);
+        NIL -> color = BLACK;
+        root = NULL;
+    }
+
+    void insert(int x){
+        if (root == NULL){
+            root = new node(x);
+            root -> color = BLACK;
+            root -> Left = root -> Right = NIL;
+        } else {
+            insert(root, x);
+        }
     }
 };
 
